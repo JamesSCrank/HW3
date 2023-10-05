61989c2c8a68e9b1bf2bf7d4de03dbe16d994d7c
JamesSCrank <jscrank@cpp.edu>
2023-10-05 16:34:36 -0700

    Current state

---
+++ script.js
@@ -0,0 +1,18 @@
+When = is pressed 
+
+if +{
+    add
+}
+if - {
+    subtract
+}
+if x {
+    multiply
+}
+if (division symbol){
+    divide
+}
+
+let msg = $first + $last = 
+
+let choice = input;
