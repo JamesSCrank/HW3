ed1e69be40ccfad3728133c071658d78d6b76988
JamesSCrank <jscrank@cpp.edu>
2023-10-05 18:07:58 -0700

    Ok

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
+<script>
+    let string = "";
+    const buttons = document.querySelectorAll('.Numero');
+    for (let .Numero of buttons){
+        button.addEventListener('click', DisplayNumber); 
+    }
+    function DisplayNumber(event) {
+        const text = document.querySelector('Display');
+        const number = event.currentTarget.innertext;
+        Number(number); 
+
+
+    }
+
+    const symbols = document.querySelectorAll('.Symbol');
+
+let number;
+let symbol; //variable to hold the math symbol; 
+
+switch(=)
+if (symbol == +){
+    output = choice1 + choice2
+}
+else if(symbol == -){
+    output = choice1 - choice2;
+}
+else if(symbol == x) {
+    output = choice1 * choice2 
+}
+else if(symbol == ÷){
+    output = choice1 / choice2
+};
+
 When = is pressed 
 
-if +{
+if "+"{
     add
 }
-if - {
+if "-" {
     subtract
 }
-if x {
+if "x" {
     multiply
 }
-if (division symbol){
+if "÷"{
     divide
 }
 
 let msg = $first + $last = 
 
 let choice = input;
+</script>
