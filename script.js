f4ed3b82f3f4a05e9e00de816bbba2cc387b9a9e
JamesSCrank <jscrank@cpp.edu>
2023-10-06 22:18:10 -0700

    I tried

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-<script>
-    let string = "";
+
+  function DisplayNumber(DisplayNumber){
+    var x = DisplayNumber.value;
+    document.getElementsByClassName("Display").innerHTML = x; 
+  }
+  
+
+
+    
+<script src="script.js">
+  const input = document.querySelector("input");
     const buttons = document.querySelectorAll('.Numero');
-    for (let .Numero of buttons){
-        button.addEventListener('click', DisplayNumber); 
+    for (let button of buttons){
+        button.addEventListener('click', DisplayNumber){
+            inputNumber(this.Numero)
+        }
     }
-    function DisplayNumber(event) {
-        const text = document.querySelector('Display');
+
+function DisplayNumber(event) {
+        const text = document.querySelector('.Display');
         const number = event.currentTarget.innertext;
         Number(number); 
 
-
-    }
-
-    const symbols = document.querySelectorAll('.Symbol');
+   const symbols = document.querySelectorAll('.Symbol');
 
 let number;
 let symbol; //variable to hold the math symbol; 
@@ -49,4 +58,3 @@ if "÷"{
 let msg = $first + $last = 
 
 let choice = input;
-</script>
